d75e3198c8b534925d0396b4ad2ca1f093da9959
Rahul Reddy <rahulreddy@Rahuls-MacBook-Pro.local>
2018-05-06 18:22:20 -0400

    added id field to grocery item post response

--- server/api/usergroceryitem.js
+++ server/api/usergroceryitem.js
@@ -6,8 +6,10 @@ var usergroceryitem = require('../models/usergroceryitem');
 router.get('/', (req, res) => {
     var user_id = req.query["user_id"]
     console.log("fetching usergroceryitem for user: " + user_id)
-    usergroceryitem.find({"user_id": user_id}, (err, result) => {
-        if(err) {
+    usergroceryitem.find({
+        "user_id": user_id
+    }, (err, result) => {
+        if (err) {
             res.status(400);
             console.log(err)
         } else {
@@ -24,20 +26,23 @@ router.post('/', (req, res) => {
     var cost = req.body["cost"]
     var expiry = req.body["expiry"]
     var item = new usergroceryitem({
-        product_id: product_id, 
-        user_id: user_id, 
+        product_id: product_id,
+        user_id: user_id,
         product_name: product_name,
         cost: cost,
         expiry: expiry
     })
-console.log(JSON.stringify(item))
-    //http://localhost:8077/api/usergroceryitem?user_id=24&product_id=96&product_name=milkwash&cost=1.99&expiry=2018-06-06T22:04:23.581Z
+    console.log(JSON.stringify(item))
+
     item.save((err) => {
-        if(err) {
+        if (err) {
             res.status(400).send(err)
         }
 
-        res.status(200).json({"msg": "user grocery item created"})
+        res.status(200).json({
+            "msg": "user grocery item created",
+            "id": item._id
+        })
     })
 });
 
